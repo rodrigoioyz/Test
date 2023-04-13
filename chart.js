aeda3473dd73484f5e3d5980d34f3cd1214344e8
rodrigoioyz <67395460+rodrigoioyz@users.noreply.github.com>
2023-04-13 14:19:18 -0400

    Update chart.js

--- chart.js
+++ chart.js
@@ -1,1 +1,12 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.2.1/chart.min.js"></script>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Gráfico con Chart.js</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+    <script src="chart.js"></script>
+  </head>
+  <body>
+    <canvas id="myChart"></canvas>
+  </body>
+</html>
