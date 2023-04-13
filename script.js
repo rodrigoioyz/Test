895af98a994aeb6117c007155615309ab02c5be5
rodrigoioyz <67395460+rodrigoioyz@users.noreply.github.com>
2023-04-13 13:30:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // Obtener el elemento canvas y el contexto 2D
 const apyChart = document.getElementById('apy-chart');
 const apyCtx = apyChart.getContext('2d');
@@ -28,10 +29,22 @@ const options = {
 // Obtener los datos del APY de Coingecko utilizando su API
 fetch('https://api.coingecko.com/api/v3/simple/price?ids=cardano&vs_currencies=usd&include_24hr_change=true')
   .then(response => response.json())
-  .then(data => {
-    // Agregar los datos al gráfico
-    data.labels = ['USD'];
-    data.datasets[0].data = [data.cardano.usd_24h_change];
+  .then(dataResponse => {
+    // Crear un nuevo objeto con los datos necesarios
+    const newData = {
+      labels: ['USD'],
+      datasets: [{
+        label: 'APY',
+        data: [dataResponse.cardano.usd_24h_change],
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgba(54, 162, 235, 1)',
+        borderWidth: 1
+      }]
+    };
+    
+    // Actualizar el objeto "data" con los nuevos datos
+    data.labels = newData.labels;
+    data.datasets[0].data = newData.datasets[0].data;
     
     // Crear el gráfico utilizando Chart.js
     const apyLineChart = new Chart(apyCtx, {
