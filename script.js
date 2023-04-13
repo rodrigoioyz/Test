12ab0ef7ef824fb0ad5d500676d7ef97da97dfe2
rodrigoioyz <67395460+rodrigoioyz@users.noreply.github.com>
2023-04-13 15:03:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-// Obtener el elemento canvas y el contexto 2D
+// Obtener el elemento canvas y el contexto 2D para el gráfico de APY
 const apyChart = document.getElementById('apy-chart');
 const apyCtx = apyChart.getContext('2d');
 
-// Configurar los datos del gráfico
-const data = {
-  labels: [], // las etiquetas se agregarán después
+// Configurar los datos y opciones iniciales del gráfico de APY
+const apyData = {
+  labels: [],
   datasets: [{
     label: 'APY',
-    data: [], // los datos se agregarán después
+    data: [],
     backgroundColor: 'rgba(54, 162, 235, 0.2)',
     borderColor: 'rgba(54, 162, 235, 1)',
     borderWidth: 1
   }]
 };
 
-// Configurar las opciones del gráfico
-const options = {
+const apyOptions = {
   responsive: true,
   maintainAspectRatio: false,
   scales: {
@@ -32,7 +31,7 @@ fetch('https://api.coingecko.com/api/v3/simple/price?ids=cardano&vs_currencies=u
   .then(response => response.json())
   .then(dataResponse => {
     // Crear un nuevo objeto con los datos necesarios para Cardano
-    const newData = {
+    const cardanoData = {
       labels: ['USD'],
       datasets: [{
         label: 'APY de Cardano',
@@ -43,25 +42,61 @@ fetch('https://api.coingecko.com/api/v3/simple/price?ids=cardano&vs_currencies=u
       }]
     };
 
-    // Actualizar el objeto "data" con los nuevos datos para Cardano
-    data.labels = newData.labels;
-    data.datasets.push(newData.datasets[0]);
+    // Actualizar los datos del gráfico de APY con los datos de Cardano
+    apyData.labels = cardanoData.labels;
+    apyData.datasets.push(cardanoData.datasets[0]);
 
-    // Actualizar el gráfico con los nuevos datos para Cardano
+    // Crear y mostrar el gráfico de APY actualizado
     new Chart(apyCtx, {
       type: 'bar',
-      data: data,
-      options: options
+      data: apyData,
+      options: apyOptions
     });
   });
 
-// Obtener los datos del precio de Bitcoin utilizando la API de CoinGecko
-fetch('https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false')
-  .then(response => response.json())
-  .then(dataResponse => {
-    // Obtener el precio actual de Bitcoin en dólares
-    const btcPrice = dataResponse.market_data.current_price.usd;
+// Obtener el elemento canvas y el contexto 2D para el gráfico de precio y volumen
+const priceChart = document.getElementById('price-chart');
+const priceCtx = priceChart.getContext('2d');
 
-    // Actualizar el contenido HTML para mostrar el precio de Bitcoin
-    document.getElementById('btc-price').innerHTML = '$' + btcPrice.toFixed(2);
-  });
+// Configurar los datos y opciones iniciales del gráfico de precio y volumen
+const priceData = {
+  labels: [],
+  datasets: [{
+    label: 'Precio',
+    data: [],
+    yAxisID: 'precio',
+    type: 'line',
+    fill: false,
+    borderColor: 'rgba(54, 162, 235, 1)',
+    borderWidth: 1
+  }, {
+    label: 'Volumen',
+    data: [],
+    yAxisID: 'volumen',
+    type: 'bar',
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgba(255, 99, 132, 1)',
+    borderWidth: 1
+  }]
+};
+
+const priceOptions = {
+  responsive: true,
+  maintainAspectRatio: false,
+  scales: {
+    xAxes: [{
+      type: 'time',
+      time: {
+        unit: 'hour'
+      }
+    }],
+    yAxes: [{
+      id: 'precio',
+      position: 'left',
+      ticks: {
+        beginAtZero: true
+      }
+    }, {
+      id: 'volumen',
+      position: 'right',
+      ticks: {
