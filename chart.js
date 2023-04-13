158d4d89ea0b59176fe9f96d6f8ece7ec02df5bd
rodrigoioyz <67395460+rodrigoioyz@users.noreply.github.com>
2023-04-13 14:34:16 -0400

    Update chart.js

--- chart.js
+++ chart.js
@@ -4,9 +4,9 @@
     <meta charset="utf-8">
     <title>Gráfico con Chart.js</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
-    <script src="chart.js"></script>
   </head>
   <body>
     <canvas id="myChart"></canvas>
+    <script src="chart.js"></script>
   </body>
 </html>
