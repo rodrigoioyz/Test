47c3b1876e4f3a7b04cf94d837d458e67702e0ab
rodrigoioyz <67395460+rodrigoioyz@users.noreply.github.com>
2023-04-13 19:14:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,70 +6,70 @@ const _ = lodash;
 
 // Crea los gráficos de TradingView
 const btcChart = new TradingView.widget(
-  {
-    "width": 980,
-    "height": 610,
-    "symbol": "COINBASE:BTCUSD",
-    "interval": "D",
-    "timezone": "Etc/UTC",
-    "theme": "dark",
-    "style": "1",
-    "locale": "en",
-    "toolbar_bg": "#f1f3f6",
-    "enable_publishing": false,
-    "allow_symbol_change": true,
-    "container_id": "btc-chart",
-    "datafeed": new UDFCompatibleDatafeed("https://demo_feed.tradingview.com"),
-  }
+{
+"width": 980,
+"height": 610,
+"symbol": "COINBASE:BTCUSD",
+"interval": "D",
+"timezone": "Etc/UTC",
+"theme": "dark",
+"style": "1",
+"locale": "en",
+"toolbar_bg": "#f1f3f6",
+"enable_publishing": false,
+"allow_symbol_change": true,
+"container_id": "btc-chart",
+"datafeed": new UDFCompatibleDatafeed("https://crypto-data-feed.com/udf/"),
+}
 );
 
 const cardanoChart = new TradingView.widget(
-  {
-    "width": 980,
-    "height": 610,
-    "symbol": "BINANCE:ADAUSDT",
-    "interval": "D",
-    "timezone": "Etc/UTC",
-    "theme": "dark",
-    "style": "1",
-    "locale": "en",
-    "toolbar_bg": "#f1f3f6",
-    "enable_publishing": false,
-    "allow_symbol_change": true,
-    "container_id": "cardano-chart",
-    "datafeed": new UDFCompatibleDatafeed("https://demo_feed.tradingview.com"),
-  }
+{
+"width": 980,
+"height": 610,
+"symbol": "BINANCE:ADAUSDT",
+"interval": "D",
+"timezone": "Etc/UTC",
+"theme": "dark",
+"style": "1",
+"locale": "en",
+"toolbar_bg": "#f1f3f6",
+"enable_publishing": false,
+"allow_symbol_change": true,
+"container_id": "cardano-chart",
+"datafeed": new UDFCompatibleDatafeed("https://crypto-data-feed.com/udf/"),
+}
 );
 
 const timeframeSelector = new TradingView.widget.selectors.Timeframe(
-  {
-    "container_id": "timeframe-selector",
-    "datafeed": new UDFCompatibleDatafeed("https://demo_feed.tradingview.com"),
-    "input_id": "timeframe-input",
-    "button_id": "timeframe-button",
-    "timeframes": [
-      { text: "1D", resolution: "D" },
-      { text: "1W", resolution: "W" },
-      { text: "1M", resolution: "M" },
-    ],
-  }
+{
+"container_id": "timeframe-selector",
+"datafeed": new UDFCompatibleDatafeed("https://crypto-data-feed.com/udf/"),
+"input_id": "timeframe-input",
+"button_id": "timeframe-button",
+"timeframes": [
+{ text: "1D", resolution: "D" },
+{ text: "1W", resolution: "W" },
+{ text: "1M", resolution: "M" },
+],
+}
 );
 
 const chartTypeSelector = new TradingView.widget.selectors.ChartType(
-  {
-    "container_id": "chart-type-selector",
-    "datafeed": new UDFCompatibleDatafeed("https://demo_feed.tradingview.com"),
-    "chart_types": [
-      { id: "1", text: "Candles", resolution: "" },
-      { id: "2", text: "Bars", resolution: "" },
-      { id: "3", text: "Line", resolution: "" },
-    ],
-  }
+{
+"container_id": "chart-type-selector",
+"datafeed": new UDFCompatibleDatafeed("https://crypto-data-feed.com/udf/"),
+"chart_types": [
+{ id: "1", text: "Candles", resolution: "" },
+{ id: "2", text: "Bars", resolution: "" },
+{ id: "3", text: "Line", resolution: "" },
+],
+}
 );
 
 const volumeIndicator = new TradingView.widget.indicators.Volume(
-  {
-    "container_id": "volume-indicator",
-    "datafeed": new UDFCompatibleDatafeed("https://demo_feed.tradingview.com"),
-  }
+{
+"container_id": "volume-indicator",
+"datafeed": new UDFCompatibleDatafeed("https://crypto-data-feed.com/udf/"),
+}
 );
