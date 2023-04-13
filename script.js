266cef49d5d48c94e8df5bd616d9c4e7107bc434
rodrigoioyz <67395460+rodrigoioyz@users.noreply.github.com>
2023-04-13 14:30:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Obtener el elemento canvas y el contexto 2D
 const apyChart = document.getElementById('apy-chart');
 const apyCtx = apyChart.getContext('2d');
@@ -20,9 +19,11 @@ const options = {
   responsive: true,
   maintainAspectRatio: false,
   scales: {
-    y: {
-      beginAtZero: true
-    }
+    yAxes: [{
+      ticks: {
+        beginAtZero: true
+      }
+    }]
   }
 };
 
@@ -44,51 +45,4 @@ fetch('https://api.coingecko.com/api/v3/simple/price?ids=cardano&vs_currencies=u
     
     // Actualizar el objeto "data" con los nuevos datos
     data.labels = newData.labels;
-    data.datasets[0].data = newData.datasets[0].data;
-    
-    // Crear el gráfico utilizando Chart.js
-    const apyLineChart = new Chart(apyCtx, {
-      type: 'line',
-      data: data,
-      options: options
-    });
-  })
-  .catch(error => console.error(error));
-
-// Obtener el elemento donde se mostrará el precio de BTC
-const btcPriceElement = document.getElementById('btc-price');
-
-// Obtener el precio actual de BTC utilizando la API de Coingecko
-fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
-  .then(response => response.json())
-  .then(data => {
-    // Mostrar el precio de BTC en el elemento correspondiente
-    btcPriceElement.innerText = `$${data.bitcoin.usd}`;
-  })
-  .catch(error => console.error(error));
-
-// Crear un gráfico de barras con datos de ejemplo
-const dataBar = {
-  labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'],
-  datasets: [{
-    label: 'Ventas',
-    data: [12, 19, 3, 5, 2],
-    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-    borderColor: 'rgba(255, 99, 132, 1)',
-    borderWidth: 1
-  }]
-};
-
-const optionsBar = {
-  scales: {
-    yAxes: [{
-      ticks: {
-        beginAtZero: true
-      }
-    }]
-  }
-};
-
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
-  type: 'bar',
+   
